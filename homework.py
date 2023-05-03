209462ae4e97f5f103b079d9e91fd324a8484079
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-03 09:28:47 +0300

    Исправления

--- homework.py
+++ homework.py
@@ -7,8 +7,10 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
     is_dark_theme = None
-    if 6 <= current_time.hour >= 22:
+    if 22 <= current_time.hour or current_time.hour <= 6:
         is_dark_theme = True
+    else:
+        is_dark_theme = False
     assert is_dark_theme is True
 
 def test_dark_theme_by_time_and_user_choice():
@@ -70,26 +72,29 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+
+def home_func(func, *args):
+    readable_txt = f'{func.__name__.replace("_", " ").title()} [{", ".join(args)}]'
+    print(readable_txt)
+    return readable_txt
+
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
+
 def open_browser(browser_name):
-    actual_result = None
-    actual_result = open_browser.__name__.replace("_", " ").title() + f" [{browser_name}]"
-    print(end='\n')
-    print(actual_result, end='\n')
+    actual_result = home_func(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
+
 def go_to_companyname_homepage(page_url):
-    actual_result = None
-    actual_result = go_to_companyname_homepage.__name__.replace("_", " ").title() + f" [{page_url}]"
-    print(actual_result, end='\n')
+    actual_result = home_func(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
+
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
-    actual_result = find_registration_button_on_login_page.__name__.replace("_", " ").title() + f" [{page_url}, {button_text}]"
-    print(actual_result, end='\n')
+    actual_result = home_func(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
